14d421f0653463bde95bfa39baf4905257c47bd5
Markus Heilig <markusheilig@gmx.net>
2019-05-23 17:23:31 +0200

    use some syntactic sugar

--- src/main/scala/sunit/TestSuite.scala
+++ src/main/scala/sunit/TestSuite.scala
@@ -5,7 +5,7 @@ class TestSuite(private val tests: Test*) extends Test {
   def run(): TestResult = {
     tests
       .map(_.run())
-      .foldLeft(TestResult(0, 0)) { case (acc, result) => acc.combine(result) }
+      .foldLeft(TestResult(0, 0))(_ combine _)
   }
 
 }
